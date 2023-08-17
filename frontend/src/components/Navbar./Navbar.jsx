6f4d87cfbec2064fc07de5e5365e3c8df1628c4c
Bishwo Bista <bishwo5bista@gmail.com>
2023-08-17 22:57:33 +0545

    refine

--- frontend/src/components/Navbar./Navbar.jsx
+++ frontend/src/components/Navbar./Navbar.jsx
@@ -4,8 +4,8 @@ const Navbar = () => {
   return (
     <>
       
-      <nav className="font-body flex items-center justify-between flex-wrap bg-orange-50 p-6">
-        <div className="flex items-center flex-shrink-0 text-stone-950 mr-6">
+      <nav className="font-body flex items-center justify-between flex-wrap bg-orange-50 py-8 px-[80px]">
+        <div className="flex items-center flex-shrink-0 text-stone-950 mr-14">
           <span className="font-semibold text-4xl tracking-tight">
             Byte Books
           </span>
@@ -15,31 +15,31 @@ const Navbar = () => {
           <div className="text-sm lg:flex-grow">
             <a
               href="#responsive-header"
-              className="block mt-4 px-5 lg:inline-block lg:mt-0  text-base font-small text-zinc-600 hover:text-zinc-950 mr-4"
+              className="block mt-4 px-4 lg:inline-block lg:mt-0  text-base font-small text-zinc-700 hover:text-zinc-950 mr-4"
             >
               Books
             </a>
             <a
               href="#responsive-header"
-              className="block mt-4 px-5 lg:inline-block lg:mt-0 text-base font-small text-zinc-600 hover:text-zinc-950 mr-4"
+              className="block mt-4 px-4 lg:inline-block lg:mt-0 text-base font-small text-zinc-700 hover:text-zinc-950 mr-4"
             >
               Categories
             </a>
             <a
               href="#responsive-header"
-              className="block mt-4 px-5 lg:inline-block lg:mt-0 text-base font-small text-zinc-600 hover:text-zinc-950 mr-4"
+              className="block mt-4 px-4 lg:inline-block lg:mt-0 text-base font-small text-zinc-700 hover:text-zinc-950 mr-4"
             >
               Blog
             </a>
             <a
               href="#responsive-header"
-              className="block mt-4 px-5 lg:inline-block lg:mt-0 text-base font-small text-zinc-600 hover:text-zinc-950 mr-4"
+              className="block mt-4 px-4 lg:inline-block lg:mt-0 text-base font-small text-zinc-700 hover:text-zinc-950 mr-4"
             >
               Wishlist
             </a>
             <a
               href="#responsive-header"
-              className="block mt-4 px-5 lg:inline-block lg:mt-0 text-base font-small text-zinc-600 hover:text-zinc-950 mr-4"
+              className="block mt-4 px-4 lg:inline-block lg:mt-0 text-base font-small text-zinc-700 hover:text-zinc-950 mr-4"
             >
               About Us
             </a>
@@ -49,7 +49,7 @@ const Navbar = () => {
             <div class="relative">
               <span class="absolute inset-y-0 left-0 flex items-center pl-3">
                 <svg
-                  class="h-5 w-5 text-gray-500"
+                  class="h-5 w-5 text-zinc-700"
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
@@ -64,8 +64,8 @@ const Navbar = () => {
               <input
                 type="search"
                 name="search"
-                placeholder="Search"
-                class="pl-10 pr-5 py-2 bg-transparent border-b border-gray-300 focus:border-black  focus:outline-none text-gray-700"
+                placeholder="Search book..."
+                class="pl-10 pr-5 py-2 bg-transparent border-b border-zinc-950 focus:border-black  focus:outline-none placeholder:text-zinc-700"
               />
             </div>
           </div>
@@ -80,7 +80,7 @@ const Navbar = () => {
                 viewBox="0 -960 960 960"
                 width="24"
                 fill="currentColor"
-                class=" text-zinc-600 hover:text-black "
+                class=" text-zinc-950 hover:text-black "
               >
                 <path d="M220-80q-24 0-42-18t-18-42v-520q0-24 18-42t42-18h110v-10q0-63 43.5-106.5T480-880q63 0 106.5 43.5T630-730v10h110q24 0 42 18t18 42v520q0 24-18 42t-42 18H220Zm0-60h520v-520H630v90q0 12.75-8.675 21.375-8.676 8.625-21.5 8.625-12.825 0-21.325-8.625T570-570v-90H390v90q0 12.75-8.675 21.375-8.676 8.625-21.5 8.625-12.825 0-21.325-8.625T330-570v-90H220v520Zm170-580h180v-10q0-38-26-64t-64-26q-38 0-64 26t-26 64v10ZM220-140v-520 520Z" />
               </svg>
@@ -90,7 +90,7 @@ const Navbar = () => {
 
             <a
               href="#"
-              className="inline-block text-sm px-4 py-2 leading-none  rounded text-zinc-600 mt-4 lg:mt-0"
+              className="inline-block text-sm px-4 py-2 leading-none  rounded text-zinc-950 mt-4 lg:mt-0"
             >
               Sign In
             </a>
