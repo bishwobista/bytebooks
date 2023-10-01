cbf7ab3207879304490350acc9e5415cf5e8462a
muruga21 <murugaperumal21062004@gmail.com>
2023-10-01 10:00:46 +0530

    added top10 component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import Navbar from "./components/Navbar./Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Hero from "./pages/Hero";
 import "./App.css";
 import Sellers from "./pages/Sellers";
+import Top10 from "./pages/Top10";
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero/>
       <Sellers/>
+      <Top10/>
       <Footer/>
     </>
   );
---
+++ src/pages/Top10.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Top10 = () =>{
+    return(
+        <div>Top 10</div>
+    )
+}
+
+export default Top10
